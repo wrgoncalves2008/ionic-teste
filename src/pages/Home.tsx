4a30bda973bfb70e2bc40c7bf4d7b246b330a005
Willians <wrgoncalves2008@gmail.com>
2020-03-01 14:23:53 -0300

    Implementado gerenciamento de estado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,26 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import React, { useState } from "react";
-import NameSetter from "../components/NameSetter";
-import NameList from "../components/NameList";
+import ContextualNameSetter from "../components/ContextualNameSetter";
+import ContextualNameList from "../components/ContextualNameList";
+import React from "react";
+// import NameSetter from "../components/NameSetter";
+// import NameList from "../components/NameList";
+
 
 const Home: React.FC = () => {
 
-  const [names, setNames] = useState<string[]>([]);
+  // const [names, setNames] = useState<string[]>([]);
 
-  const addName = (name: string) => {
-    const newNames = [name, ...names];
-    setNames(newNames);
-  }
+  // const addName = (name: string) => {
+  //   const newNames = [name, ...names];
+  //   setNames(newNames);
+  // }
 
-  const removeName = (removeIndex: number) => {
+  // const removeName = (removeIndex: number) => {
 
-    const newNames = [...names];
-    newNames.splice(removeIndex, 1);
-    setNames(newNames);
-  }
+  //   const newNames = [...names];
+  //   newNames.splice(removeIndex, 1);
+  //   setNames(newNames);
+  // }
 
   return (
     <IonPage>
@@ -28,11 +31,14 @@ const Home: React.FC = () => {
       </IonHeader>
 
       <IonContent className="ion-padding">
-        <NameSetter initialValue="Meu nome é" placeholder="Seu nome" buttonTitle="Definir seu nome?" onNameSet={addName} />
+        <ContextualNameSetter initialValue="Meu nome é" placeholder="Seu nome" buttonTitle="Definir seu nome?" />
+        <ContextualNameList />
+
+        {/* <NameSetter initialValue="Meu nome é" placeholder="Seu nome" buttonTitle="Definir seu nome?" onNameSet={addName} />
         {/* <NameSetter initialValue="Sua Mãe é" placeholder="Nome da sua mãe" buttonTitle="Definir sua Mãe" onNameSet={addName} /> */}
         {/* <NameSetter initialValue="Seu Pai é" placeholder="Nome do seu Pai" buttonTitle="Definir seu pai" onNameSet={addName} /> */}
 
-        <NameList names={names} onRemoveName={removeName} />
+        {/* <NameList names={names} onRemoveName={removeName} /> */}
 
       </IonContent>
     </IonPage>
